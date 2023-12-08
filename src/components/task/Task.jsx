18079f36b5b95cc3e91453f0eec4dd0891d54448
gabogarcia1 <gabegarcia916@gmail.com>
2023-12-08 14:09:05 -0300

    agregando cositas

--- src/components/task/Task.jsx
+++ src/components/task/Task.jsx
@@ -12,7 +12,7 @@ export const Task = ({ taskData }) => {
 
         const newTaskObj = {
             id: addDataCard.length == 0 ? 1 : addDataCard[addDataCard.length - 1].id + 1,
-            taskTitle:newTask
+            taskTitle: newTask
         }
 
         setAddDataCard([
@@ -28,21 +28,25 @@ export const Task = ({ taskData }) => {
     }
 
 
-  return (
-    <div className="container py-5" id="featured-3">
-        <h2 className="pb-2 border-bottom text-white">MIS TAREAS</h2>
-        <div className="row py-5">
-            { addDataCard.map( (data) => (
-            <TaskItem 
-                key={data.id} 
-                dataTaskItem={data} 
-                deleteTask={(id) => deleteTask(id)}
+    return (
+        <div className="container py-5" id="featured-3">
+            <h2 className="pb-2 border-bottom text-white">MIS TAREAS</h2>
+            {
+                addDataCard.length == 0 ? <h3 className="pb-2 border-bottom text-white">No Tenes tareas, hace click en agregar</h3> : <div className="row py-5">
+                    {addDataCard.map((data) => (
+                        <TaskItem
+                            key={data.id}
+                            dataTaskItem={data}
+                            deleteTask={(id) => deleteTask(id)}
+                        />
+                    ))}
+                </div>
+            }
+
+
+            <TaskAdd
+                onClickAdd={(newTask) => onClickAdd(newTask)}
             />
-            ))}
         </div>
-        <TaskAdd 
-            onClickAdd={(newTask) => onClickAdd(newTask)}
-        />
-    </div>
-  )
+    )
 }
